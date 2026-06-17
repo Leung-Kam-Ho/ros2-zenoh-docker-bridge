b6151fd2b9e4893eb7848a655bb71348a944536e
Leung-Kam-Ho <kamho.leung@polyu.edu.hk>
2026-06-17 22:56:29 +0800

    Update run_r2_jazzy.sh: set DISPLAY environment variable and adjust Docker run options

--- scripts/run_r2_jazzy.sh
+++ scripts/run_r2_jazzy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -euo pipefail
+export DISPLAY=:0
+xhost +
 
 NAME=${1:-r2_jazzy}
 DOMAIN_ID=${2:-0}
@@ -8,7 +10,9 @@ DISPLAY_VAL="${DISPLAY:-host.docker.internal:0}"
 docker run -it --rm \
     --name "$NAME" \
     --network host \
-    -e DISPLAY="$DISPLAY_VAL" \
+    -e DISPLAY="docker.for.mac.host.internal:0" \
     -e ROS_DOMAIN_ID="$DOMAIN_ID" \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -e LIBGL_ALWAYS_INDIRECT=1 \
+    -e QT_XCB_GL_INTEGRATION=none \
     r2_jazzy
+    
